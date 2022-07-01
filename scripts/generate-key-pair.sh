7789abbf45a33f7349bb7ac72622cc9821a7abca
v0xpopuli <vadim.rozhkalns@gmail.com>
2022-07-01 13:27:25 +0300

    Makefile update

--- scripts/generate-key-pair.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p "configs/keys"
-
-openssl genrsa -out configs/keys/rsa 4096
-openssl rsa -in configs/keys/rsa -pubout -out configs/keys/rsa.pub
--- scripts/generate-proto.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker pull namely/protoc-all
-
-docker run -v $1:/defs namely/protoc-all -d ./ -l go -o ./
-
-mv `pwd`/protobuf/generated/* `pwd`/internal/generated/
-rm -rf `pwd`/protobuf/generated
